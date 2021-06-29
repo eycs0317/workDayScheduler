756e8c3b88fa7332532632e922225fc41740ba79
Eddie Yeung <eycs0317@gmail.com>
2021-06-29 16:23:00 -0700

    finish html layout, working on check tie to set colors

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var today = moment()
 
 $('#currentDay').text(today.format('dddd, MMMM Do'))
+$("textarea").addClass('present');
+
+
+var currentHrs = Number(today.format('HH'))
+console.log(typeof currentHrs)
