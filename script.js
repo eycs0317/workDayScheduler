d804931eb9204034d6cb784de9dda10fe0fd500d
Eddie Yeung <eycs0317@gmail.com>
2021-06-29 17:19:23 -0700

    complete setColorPerHours function

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 var today = moment()
 
 $('#currentDay').text(today.format('dddd, MMMM Do'))
-$("textarea").addClass('present');
 
 
-var currentHrs = Number(today.format('HH'))
-console.log(typeof currentHrs)
+
+
+
+//check current hrs and set the color
+function setColorPerHours () {
+  //var currentHrs = Number(moment().format('HH'))
+  //console.log(currentHrs) //number of 16
+  var currentHrs = 13
+
+  $( "h4" ).each(function( index ) {
+    var hourBlockNum = parseInt($(this).attr('id'))
+    if(hourBlockNum < currentHrs) {
+      $(this).siblings('textarea').addClass('past')
+    } else if (hourBlockNum === currentHrs) {
+      console.log('present')
+      $(this).siblings('textarea').addClass('present')
+    } else {
+      $(this).siblings('textarea').addClass('future')
+      console.log('future')
+    }
+  });
+}
+
+setColorPerHours()
